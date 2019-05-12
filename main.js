dac131c8939dcc9da6c6ea2f152e96ea20504f0a
dooseopkim <entjqcjswo@gmail.com>
2019-05-12 22:00:50 +0900

    Create basic layout by pug

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
 var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+var session = require("express-session");
+var FileStore = require("session-file-store")(session);
+
 var app = express();
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(
+  express.static("public", {
+    setHeaders: function(res, path, stat) {
+      res.set("x-timestamp", Date.now());
+    }
+  })
+);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get("/", function(req, res, next) {
-  res.send("Hello World");
+  res.render("common", { content: "login", dummy: "qwerty" });
 });
-app.listen(3000);
+app.listen(3002);
